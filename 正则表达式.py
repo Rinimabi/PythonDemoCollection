69c8c15a3dabd6a7fad992ab3c19ec9a0cf62c54
weiyongxuan <weiyongxuan@ly-sky.com>
2019-12-12 17:34:34 +0800

    正则表达式

--- 正则表达式.py
+++ 正则表达式.py
@@ -1,3 +1,5 @@
 import re
 print(re.match('www', 'www.runoob.com').span())  # 在起始位置匹配
-print(re.match('com', 'www.runoob.com'))         # 不在起始位置匹配
+print(re.match('www.runoob.com', 'www.runoob.com'))         # 不在起始位置匹配
+
+print(re.match('html', '<html>www.runoob.com</html>'))         # 不在起始位置匹配
